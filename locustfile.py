8e7fc1e5d5f6c48dcf54e03ecd9a1bb18f4e4f4f
kolo246 <pgrzesiak26@gmail.com>
2020-04-01 15:57:04 +0200

    add Locust test

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,20 @@
 from locust import HttpLocust, TaskSet, task
+import os
 
 class MyTaskSet(TaskSet):
-    @task
-    def index(self):
-        self.client.get("/")
+
+    @task(3)
+    def upload(self):
+        imageFile = os.path.join(os.path.dirname(__file__),'test_image/DSC_0015.JPG')
+        self.client.post("/upload", files={'file':open(imageFile,'rb')}, verify=False)
+
+    @task(2)
+    def blurImage(self):
+        self.client.get("/blur_image")
+
+    @task(1)
+    def reset(self):
+        self.client.get("/reset")
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
