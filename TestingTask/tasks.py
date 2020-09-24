f7211c7c2ddfc1793e0ebcecbdac9920e0c0093c
kolo246 <pgrzesiak26@gmail.com>
2020-09-24 16:05:29 +0200

    add test_image

--- TestingTask/tasks.py
+++ TestingTask/tasks.py
@@ -1,14 +1,12 @@
 from locust import HttpLocust, TaskSet, task
 import os
 
-
 class BlurTaskSet(TaskSet):
 
     @task(1)
     def blur(self):
-        imageFile = os.path.join(os.path.dirname(__file__),'/locust-tasks/mylluur.jpg')
+        imageFile = os.path.join(os.path.dirname(__file__),'/locust-tasks/test_image.jpg')
         self.client.post("/blur", files={'file':open(imageFile,'rb')}, verify=False)
             
-
 class BlurLocust(HttpLocust):
     task_set = BlurTaskSet
