ec9ef5ac5bfdbbe64eabdf9fafd593954fadb33e
kolo246 <pgrzesiak26@gmail.com>
2020-09-29 20:38:34 +0200

    add responseError

--- TestingTask/tasks.py
+++ TestingTask/tasks.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, task, ResponseError
 import os
 
 class BlurTaskSet(TaskSet):
@@ -6,7 +6,10 @@ class BlurTaskSet(TaskSet):
     @task(1)
     def blur(self):
         imageFile = os.path.join(os.path.dirname(__file__),'/locust-tasks/test_image.jpg')
-        self.client.post("/blur", files={'file':open(imageFile,'rb')}, verify=False)
+        with open(imageFile,'rb') as file:
+            with self.client.post("/blur", files={'file':file}, verify=False) as response:
+                if response.status_code != 200:
+                    raise ResponseError("Request failed")
             
 class BlurLocust(HttpLocust):
     task_set = BlurTaskSet
